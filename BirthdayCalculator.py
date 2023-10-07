f3a28f991504a0c328bd333cf80150d5c6c0020c
Ajadi Ademola <95619519+Elijah-Ajadi@users.noreply.github.com>
2023-10-07 06:44:03 -0700

    Create BirthdayCalculator.py

---
+++ BirthdayCalculator.py
@@ -0,0 +1,15 @@
+# This is  a birthday calculator 
+
+print("Welcome to this age calculator,")
+name = input("Enter your name please: ")
+
+year =  int(input(f" {name} Enter your birth year: "))
+
+#  cy = Current year
+
+cy = 2023
+
+age = cy - year
+
+print(f"{name}, your current age is {age}")
+
